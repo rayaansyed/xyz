dd7459fe0e8d95e9681007fe185dc9652048103e
rayaansyed <132567328+rayaansyed@users.noreply.github.com>
2023-05-09 02:50:19 -0700

    Create pyt.py

---
+++ pyt.py
@@ -0,0 +1,12 @@
+a=34
+b=57
+c=90
+div=a/b
+sum=a+b+c
+dif=a-b-b
+mul=a*b*c
+print("the division of a and b is", div)
+print("the addition of 3 numbers is", sum)
+print("the difference of the 3 numbers is", dif)
+print("the multiplication of 3 numbers are", mul)
+ 
