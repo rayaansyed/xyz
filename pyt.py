4d3d56b88895b618af0b97d6697f9ca6d1f2c972
tcs <syedrayaan14@gmail.com>
2023-05-11 03:49:42 -0700

    hello good morning

--- pyt.py
+++ pyt.py
@@ -4,8 +4,9 @@ c=90
 div=a/b
 sum=a+b+c
 dif=a-b-b
+mul=a*b*c
 print("the division of a and b is", div)
 print("the addition of 3 numbers is", sum)
 print("the difference of the 3 numbers is", dif)
-
+print("the multiplication of 3 numbers are", mul)
  
